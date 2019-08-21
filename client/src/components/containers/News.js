1642dee7bc37dc3df8b817823faa584e71b4bf3d
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2019-08-21 09:08:03 +0100

    news item listings

--- client/src/components/containers/News.js
+++ client/src/components/containers/News.js
@@ -1,8 +1,17 @@
 import React, {Component} from 'react';
+import NewsItemListing from '../presentation/NewsItemListing';
 
 class News extends Component {
     render() {
-        return ( <div> Las Noticias </div> );
+        return (
+            <div>
+                <h2> Las Noticias </h2>
+                <ul>
+                <li><NewsItemListing id="1" title="Buho Loco Tortura a Conductores" teaser="Se ha visto un buho que caza a los coches en Zaragoza" /></li>
+                <li><NewsItemListing id="1" title="Buho Roba Cartas" teaser="Unas cartas han desaparecido en Tarragona -  Un buho es sospechoso." /></li>
+                </ul>
+            </div>
+        );
     }
 }
 
