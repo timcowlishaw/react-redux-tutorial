8d285469ee8bec66a7a6da25cbc416885746a95f
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2019-08-21 08:44:42 +0100

    home component

---
+++ client/src/components/layouts/Home.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return ( <div> Welcome to MadOwlNews.com </div> );
+    }
+}
+
+export default Home;
