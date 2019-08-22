060dd7a1cad2ee567a275a1abfa25fba86ffac86
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2019-08-22 08:46:26 +0100

    add nav

--- client/src/components/layouts/Layout.js
+++ client/src/components/layouts/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Layout extends Component {
     render() {
@@ -6,6 +7,12 @@ class Layout extends Component {
             <div>
                 <div>
                     <h1>NoticiasBuhoLoco.com  - Noticias de ultima hora sobre los buhos locos</h1>
+                </div>
+                <div>
+                    <ul>
+                        <li><Link to={'/'}>Inicio</Link></li>
+                        <li><Link to={'/about'}>Quien somos?</Link></li>
+                    </ul>
                 </div>
                 <div>
                     { this.props.children } 
